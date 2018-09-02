76c38ce006a05d0f99a407b8e2bc1b0b0538e5aa
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-02 17:14:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 function happyHolidays()
  {return "Happy Holidays!"
  }// write your code below!}}
-function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
-}
-function happyHolidayto (holiday, name){
-  return 'Happy ${holiday}, ${name}'
-}
-function holidayCountdown (days, holiday) {
-  return 'It'\s ${days} days until ${holiday}
-}
